23a67a76df3100e332e87580c418dbda9172cad7
gastrodia <ipv6.elata@gmail.com>
2014-03-27 18:16:23 +0800

    rockmongo

---
+++ mongo.sh
@@ -0,0 +1,10 @@
+sudo apt-get install mongodb
+sudo apt-get install apache2 php5 php5-dev php5-cli
+cd /var/www
+sudo wget http://rock-php.googlecode.com/files/rockmongo-v1.0.11.zip
+sudo unzip rockmongo-v1.0.11.zip
+sudo apt-get install php-pear
+sudo pecl install mongo
+sudo echo extension=mongo.so >> /etc/php5/apache2/php.ini
+sudo service apache2 restart 
+
